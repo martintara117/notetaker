1c49d974490d5e776d5e1800ccc7bb8a4e93cf04
Tara Martin <martin.tara117@gmail.com>
2020-11-15 10:04:24 -0500

    created paths for htmls and a post for adding new notes, giving the note an id

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require("fs");
+const path = require("path");
+const { networkInterfaces } = require("os");
 
 app.use(
   express.urlencoded({
@@ -14,6 +17,30 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("/api/notes", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./db/db.json"));
+  res.json(data);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile("notes.html", { root: path.join(__dirname, "public") });
+});
+
+app.post("/api/notes", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNote = req.body;
+  newNote.id = Date.now();
+  data.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(data));
+  res.sendStatus(200);
+});
+
+app.delete("/api/notes/:id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`server is running on http:// local host: ${PORT}`);
